129f6e79f1932f4b564c01301f459e8c7bb480d8
Sara Kane <kane.sarak@gmail.com>
2020-06-22 14:19:08 -0700

    Add methods to count the number of words, the number of vowels, and the number of consonants
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -2,3 +2,29 @@ export function Entry(title, body) {
   this.title = title;
   this.body = body;
 }
+
+Entry.prototype.numberOfWords = function(){
+  return this.body.split(" ").length;
+}
+
+Entry.prototype.numberOfVowels = function(){
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let vowelCount = 0;
+  for(let char of this.body.toLowerCase()) {
+    if(vowels.includes(char)){
+      vowelCount++;
+    }
+  }
+  return vowelCount;
+}
+
+Entry.prototype.numberOfConsonants = function(){
+    let consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M',' N', 'P',' Q',' R', 'S', 'T', 'V', 'X', 'Z', 'W','Y'];
+    let consonantCount = 0;
+    for(let char of this.body.toUpperCase()) {
+      if(consonants.includes(char)){
+        consonantCount++;
+      }
+    }
+    return consonantCount;
+  }
